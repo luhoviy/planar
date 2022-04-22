f9edf8babb8796a3a8997d9300fd6bead884cc9b
ruslan-luhoviy <ruslan.luhoviy@intellias.com>
2022-04-22 14:28:48 +0300

    app header / base routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'me',
+    loadChildren: () => import('./routes/profile/profile.module').then((m) => m.ProfileModule),
+    data: { editable: true }
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('./routes/profile/profile.module').then((m) => m.ProfileModule),
+    data: { editable: false }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,32 @@ import { AppComponent } from './app.component';
 import { AppStoreModule } from './store/app-store.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_CONFIG, AppConfigFactory } from './shared/injectors/app-config';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AppStoreModule, HttpClientModule, MatButtonModule],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AppStoreModule,
+    HttpClientModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatIconModule
+  ],
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useFactory: AppConfigFactory
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
